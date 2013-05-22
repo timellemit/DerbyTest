082323a973de8124afa17bedbfd74c17f5d7b846
Yorko <foofest@gmail.com>
2013-05-22 11:48:32 +0400

    Добавление полей

--- Derby/src/test/Test.java
+++ Derby/src/test/Test.java
@@ -1,22 +1,100 @@
 package test;
-import java.sql.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+public class Test extends JPanel {
+    private boolean DEBUG = false;
 
-public class Test {
-	public static void main(String[] args) {
-		java.util.Date javaDate = new java.util.Date("2013/03/22");
-        long t = javaDate.getTime();
-        System.out.println("2013/03/22".replace('/', '-'));
-        java.sql.Date sqlDate = new java.sql.Date(t);
-        System.out.println(sqlDate);
-        java.sql.Date sqlDate2 = new java.sql.Date(t);
-        
-        SimpleDateFormat df = new SimpleDateFormat("2005-03-21");
-    
-        
-	}
+    public Test() {
+        super(new GridLayout(1,0));
 
+        String[] columnNames = {"First Name",
+                                "Last Name",
+                                "Sport",
+                                "# of Years",
+                                "Vegetarian"};
+
+        Object[][] data = {
+	    {"Kathy", "Smith",
+	     "Snowboarding", new Integer(5), new Boolean(false)},
+	    {"John", "Doe",
+	     "Rowing", new Integer(3), new Boolean(true)},
+	    {"Sue", "Black",
+	     "Knitting", new Integer(2), new Boolean(false)},
+	    {"Jane", "White",
+	     "Speed reading", new Integer(20), new Boolean(true)},
+	    {"Joe", "Brown",
+	     "Pool", new Integer(10), new Boolean(false)}
+        };
+
+        final JTable table = new JTable(data, columnNames);
+        table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+        table.setFillsViewportHeight(true);
+
+        if (DEBUG) {
+            table.addMouseListener(new MouseAdapter() {
+                public void mouseClicked(MouseEvent e) {
+                    printDebugData(table);
+                }
+            });
+        }
+
+        //Create the scroll pane and add the table to it.
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        //Add the scroll pane to this panel.
+        add(scrollPane);
+    }
+
+    private void printDebugData(JTable table) {
+        int numRows = table.getRowCount();
+        int numCols = table.getColumnCount();
+        javax.swing.table.TableModel model = table.getModel();
+
+        System.out.println("Value of data: ");
+        for (int i=0; i < numRows; i++) {
+            System.out.print("    row " + i + ":");
+            for (int j=0; j < numCols; j++) {
+                System.out.print("  " + model.getValueAt(i, j));
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------");
+    }
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("SimpleTableDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create and set up the content pane.
+        Test newContentPane = new Test();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
 }
